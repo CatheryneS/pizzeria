518efa1274242693b94877f994c3a5203c8e9b42
Catheryn Small <catheryn.small@gmail.com>
2020-10-18 14:40:12 -0400

    validate user attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,18 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
+        byebug
     end
 
     def create
-        @user = User.create(user_params)
-        redirect_to user_path(@user) 
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+            byebug
+        end 
     end
 
     def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true, email: true
+    validates :phone, presence: true
+
 end
