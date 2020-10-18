3b977955cc5b0f68a0c50389606a31ff7ef4eb47
Catheryn Small <catheryn.small@gmail.com>
2020-10-18 01:13:48 -0400

    create user sign up form and instance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+     before_action :find_user, only: [:show, :edit, :update, :delete]
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user) 
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone)
+    end
 end
