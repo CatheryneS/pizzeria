6e4e02b193bd7b7c745c6c756b804ee0997ce6c4
Catheryn Small <catheryn.small@gmail.com>
2020-10-25 19:24:14 -0400

    complete CRUD for orders model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,10 @@ class OrdersController < ApplicationController
 
     def create 
         @order = current_user.orders.build(order_params)
-
+        @order.status = "In Progress"
         if @order.valid?
             @order.save
+byebug
             flash[:success] = "Your order has been placed."
             redirect_to user_orders_path(@order.user_id)
         else
@@ -26,15 +27,24 @@ class OrdersController < ApplicationController
     end
 
     def update
+        if @order.update(order_params)
+            flash[:success] = "Order notes have been updated."
+            redirect_to order_path(@order)
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @order.destroy
+        flash[:success] = "Your order has been cancelled."
+        redirect_to user_orders_path(current_user)
     end
 
     private
 
     def order_params
-        params.require(:order).permit(:note, pizza_ids: [], pizzas_attributes: [:name, :description, :user_id])
+        params.require(:order).permit(:note, pizza_ids: [], pizzas_attributes: [:name, :description, :user_id, :id])
     end
 
     def find_order
