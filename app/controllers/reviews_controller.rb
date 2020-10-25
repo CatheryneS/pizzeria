acad8af0217b534619fcf5b221097724e8a62940
Catheryn Small <catheryn.small@gmail.com>
2020-10-24 21:30:43 -0400

    create review form, links, and index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+    # before_action :require_login
+
+    def index
+        if params[:pizza_id] && @pizza = Pizza.find_by(id: params[:pizza_id])
+            @reviews = @pizza.reviews
+        else
+            @reviews = Reviews.all
+        end
+    end
+
+    def new
+        if params[:pizza_id] && @pizza = Pizza.find_by(id: params[:pizza_id])
+            @review = @pizza.reviews.build
+        else
+            @review = Review.new
+        end
+    end
+
+    def create
+        byebug
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
 end
