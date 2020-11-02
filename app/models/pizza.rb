fcbfee5286369413009d49acb196277ff92fee2f
Catheryn Small <catheryn.small@gmail.com>
2020-11-01 23:47:58 -0500

    set associations for pizzas and toppings

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,6 +3,8 @@ class Pizza < ApplicationRecord
     has_many :order_pizzas
     has_many :orders, through: :order_pizzas
     has_many :reviews, dependent: :destroy
+    has_many :pizza_toppings
+    has_many :toppings, through: :pizza_toppings
 
     validates :description, presence: true
     validates :name, presence: true, uniqueness: true
