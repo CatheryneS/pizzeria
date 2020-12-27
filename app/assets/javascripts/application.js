3c3c59f0880cfab74dcfaa0f84e5e4bc2673455d
Catheryn Small <catheryn.small@gmail.com>
2020-12-27 02:16:59 -0500

    add hero, scroll function, and pizza

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+
+mybutton = document.getElementById("scroll");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
